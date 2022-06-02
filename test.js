ef1a99fd68ff8e74d15f2776a5431dc8af16701a
Jakub Bedynek <kuba.xray@gmail.com>
2022-06-02 18:42:50 +0200

    Learned about "This" keyword

--- test.js
+++ test.js
@@ -1,19 +1,14 @@
-var myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.driver = "Shaun";
-myCar.drive = function(){console.log("now driving");};
-
-console.log(myCar.driver);
-
-myCar.drive();
-
 var myCar2 = {
     maxSpeed: 70,
     driver: "Net Ninja",
     drive: function(speed, time){
         console.log(speed * time);
+    },
+    logDriver: function(){
+        console.log("driver name is " + this.driver);
     }
 };
 
+myCar2.logDriver();
 console.log(myCar2.maxSpeed);
 myCar2.drive(50, 3);
