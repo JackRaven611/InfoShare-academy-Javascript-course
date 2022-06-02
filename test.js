d68b9183b5105b1bf0ebdd420d2c2491d55cdc05
Jakub Bedynek <kuba.xray@gmail.com>
2022-06-02 18:09:28 +0200

    Learned about slicing and splitting strings

--- test.js
+++ test.js
@@ -1,4 +1,12 @@
-var string1 = "Abc";
-var string2 = "Bcd";
+//slicing
+var str = "hello, world";
+var str2 = str.slice(2, 9);
 
-console.log(string1 < string2);
+console.log(str2);
+
+//spliting
+var tags = "meat, ham, salami, pork, beef, chicken";
+
+var tagsArray = tags.split(",");
+
+console.log(tagsArray)
